77c0c31634e16a33cf4ad9c6ec28d0b01a90c80b
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 14:32:31 +0100

    Exercice 01 compété !

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ex 01
+            int a = 10;
+            int b = 5;
+            int resultat = a + b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+            Console.WriteLine(a + "+" + b + "=" + resultat);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
